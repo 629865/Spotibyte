0e1b2ef3b4c278f383af4ee12e0a12dbf0ecbc97
Justin Lambdin <43350383+629865@users.noreply.github.com>
2020-03-13 13:10:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -26,8 +26,67 @@ app.get("/items", (req, res) => {
         res.send(rows);
     });
 })
+app.get("/songs", (req, res) => {
+    let sql = `SELECT * FROM songs`;
 
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+          //nothing for each row
+        });
+        console.log("GET /songs - songs sent to user");
+        res.send(rows);
+    });
+    })
 app.post("/login", (req, res) => {
+    const user = req.body.user
+    const sql = 'SELECT userid FROM users WHERE password = ? AND email = ?'
+    const values = [user.first, user.last, user.email, user.password]
+    let userID
+     db.all(sql, values, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+         if (rows.length == 0) {
+             res.status(404)
+            res.json({
+                message: 'No such user',
+                userID: null
+    }) 
+         }
+         else {
+             res.status(200)
+            res.json({
+                message: 'User logged in!',
+                userID: rows[0].userID
+    }) 
+         }
+        });
+})
+
+app.post("/song", (req, res) => {
+    const song = req.body.song
+    const sql = `INSERT INTO songs 
+    (name) 
+    VALUES (?)`
+    const values = [song.name]
+    let userID
+    db.run(sql, values, function (err) {
+        if (err)
+            console.log(err)
+        else {            
+            console.log(`userid ${this.lastID} created`)
+        }
+    })
+    
+    res.json({
+        message: 'User logged in',
+        userID: userID 
+    })
+})
+app.post("/createAccount", (req, res) => {
     const user = req.body.user
     const sql = `INSERT INTO users 
     (firstname, lastname, email, password) 
--- public/script.js
+++ public/script.js
@@ -1,29 +1,31 @@
-const $itemsContainer = document.querySelector("section#items")
+const $songsContainer = document.querySelector("section#songs")
 const $cart = document.querySelector("section#cart ul")
-let items = []
+let songs = []
 let inCart = []
 
-loadItems()
+loadSongs()
 
-function loadItems() {
-    fetch("/items")
+
+function loadSongs() {
+    fetch("/songs")
         .then( response => response.json() )
         .then( response => {
         console.log(response)
-            createItemCards(response) 
+            createItemSongs(response) 
         })
         .catch(err => console.error(err))
 }
 
-function createItemCards(_items) {
-    items = _items
-    const itemsHTML = _items.map(item => 
-        `<div class="item">
-            <h3>Name: ${item.name}</h3>            
-            <button onClick="addToCart(${item.itemid}, event)">Add to Cart</button>
+function createItemSongs(_songs) {
+    songs = _songs
+    const songsHTML = _songs.map(song => 
+        `<div class="song">
+            <h3>Name: ${song.name}</h3>            
+            <button onClick="addToCart(${song.songid}, event)">Add to Cart</button>
+<button onClick="addToCart(${song.songid}, event)">Add to Cart</button>
         </div>`
     ).join('')
-    $itemsContainer.innerHTML = itemsHTML    
+    $songsContainer.innerHTML = songsHTML    
 }
 
 function login(event) {
@@ -52,14 +54,62 @@ function login(event) {
         .catch(err => console.error(err))
 
 }
+function song(event) {
+    event.preventDefault()
+    //create order object
+    const $form = document.forms[0]
+    const order = {
+        song: {
+            name: $form.name.value,
+        }
+    }
+    //POST on /login
+    const config = {
+        method: "POST",
+        body: JSON.stringify( order ),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    }
+    fetch("/song",config)
+        .then( response => response.json() )
+        .then( response => loadSongs() )
+        .catch(err => console.error(err))
 
+}
+function createAccount(event) {
+    event.preventDefault()
+    //create order object
+    const $form = document.forms[0]
+    const order = {
+        createAccount: {
+            first: $form.first.value,
+            last: $form.last.value,
+            email: $form.email.value,
+            password: $form.password.value
+        }
+    }
+    //POST on /login
+    const config = {
+        method: "POST",
+        body: JSON.stringify( order ),
+        headers: {
+            "Content-Type":"application/json"
+        }
+    }
+    fetch("/createAccount",config)
+        .then( response => response.json() )
+        .then( response => loadSongs() )
+        .catch(err => console.error(err))
+
+}
 function addToCart(id, event) {
-    const item = items.find(item => item.itemid == id)
+    const song = songs.find(song => song.songid == id)
 
-    const $newItem = document.createElement("li")
-    $newItem.innerHTML = 
-        `${item.name}`
-    $cart.append($newItem)
-    inCart.push(item)
-    document.querySelector("span#itemCount").innerHTML = inCart.length
+    const $newsong = document.createElement("li")
+    $newSong.innerHTML = 
+        `${song.name}`
+    $cart.append($newSong)
+    inCart.push(song)
+    document.querySelector("span#songCount").innerHTML = inCart.length
 }
